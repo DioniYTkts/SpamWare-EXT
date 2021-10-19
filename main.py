608e652fdac8cf48e6ba0bd3290eb216ed5fbdb8
DioniYTkts <78500087+DioniYTkts@users.noreply.github.com>
2021-10-19 14:59:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+repeats = " VIRUS " * 9000000
+
+input("installing virus...")
+
+input("do you want to remove the virus?")
+
+input("oh no i cant do anything")
+
+print("deleting system files")
+
+input("hahaha your device is now encrypted.")
+
+input("sending personal files to Moscow Russia...")
+
+print(repeats)
